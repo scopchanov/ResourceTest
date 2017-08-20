284c7bfb5287d499e57b335530a8693efccfb1d3
Michael Scopchanov <michael_scopchanov@yahoo.com>
2017-08-20 02:39:48 +0200

    Added debug message.

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
 
 namespace Ui {
 class MainWindow;
@@ -10,7 +11,6 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
 	Q_OBJECT
-
 public:
 	explicit MainWindow(QWidget *parent = 0);
 	~MainWindow();
